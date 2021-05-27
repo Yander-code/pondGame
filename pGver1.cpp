ff1eb3729c4903d71a530d2070e0d727bb8a9a56
Yander-code <derek.yen77@gmail.com>
2021-05-27 22:00:45 +0800

    map almost done

--- pGver1.cpp
+++ pGver1.cpp
@@ -42,19 +42,31 @@ int main(){
 		std::string temp;
 		std::cin >> temp;
 		aLoc += dice(6);
-		if(lastLoc!=0){
+		if(lastLoc!=0 && (lastLoc2)%25*2!=(aLoc)%25*2){
 			map[(lastLoc)%25*2]='O';
+		}else if((lastLoc)%25*2==(aLoc)%25*2){
+			map[(lastLoc)%25*2]='B';
+		}
+		if(lastLoc%25==0){
+			map[0]='S';
 		}
 		map[(aLoc)%25*2]='A';
 		lastLoc=aLoc;
 		printMap();
+		std::cout<<std::endl;
+		
 		
 		std::cout << "輪到B玩家擲骰子(請按任意按鍵): \n";
 	
 		std::cin >> temp;
 		bLoc += dice(6);
-		if(lastLoc2!=0){
+		if(lastLoc2!=0 && (lastLoc2)%25*2!=(bLoc)%25*2){
 			map[(lastLoc2)%25*2]='O';
+		}else if((lastLoc)%25*2==(bLoc)%25*2){
+			map[(lastLoc2)%25*2]='A';
+		}
+		if(lastLoc%25==0){
+			map[0]='S';
 		}
 		map[(bLoc)%25*2]='B';
 		lastLoc2=bLoc;
