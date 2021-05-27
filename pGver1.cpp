63060190d04bc5b07de7f6f1c77e460e3ad66cc5
Yander-code <66255028+Yander-code@users.noreply.github.com>
2021-05-27 12:06:34 +0800

    lineVer.

--- pGver1.cpp
+++ pGver1.cpp
@@ -3,7 +3,7 @@
 #include <ctime>
 #include <string>
 
-std::string mapf, mapl, map2, map3, map4;
+std::string map;
 
 void gameIntro(){
 	std::cout << "圖示說明: A:A玩家的位置 B:B玩家的位置 ○:埤塘" << std::endl; 
@@ -12,70 +12,54 @@ void gameIntro(){
 int dice(int k){
 	srand(time(0));
 	int num = rand()%k+1;
-    std::cout << "骰子點數: " << num << std::endl;
+    std::cout << "骰子點數: " << num << std::endl << std::endl;
 
     return num;
 }
 void printMap(){
-	std::cout << mapf <<"\n\n"<<map2<<"\n\n"<<map3<<"\n\n"<<map4<<"\n\n"<<mapl<<"\n";
+	std::cout << map <<"\n";
 } 
 int generateOriginalMap(int n){
 	
-	mapf="※ ";
+	map="S ";
 	for(int i=0; i<n-1; i++){
-		mapf += "○ ";
+		map += "O ";
 	} 
-	for(int i=0; i<n; i++){
-		mapl += "○ ";
-	} 
-	map2="○";
-	map3="○";
-	map4="○";
-	for(int i=0; i<(n-2)*3+1; i++){
-		map2 += " ";
-		map3 += " ";
-		map4 += " ";
-	} 
-	map2+="○";
-	map3+="○";
-	map4+="○";
 } 
 
 int main(){
 	gameIntro();
 	
-	generateOriginalMap(6);
+	generateOriginalMap(25);
 
 	printMap();
 	
 	int aMoney=0, bMoney=0;
 	int aLoc=0, bLoc=0;
-	int lastLoc=0; 
+	int lastLoc=0, lastLoc2=0; 
 	while(1){
 		std::cout << "輪到A玩家擲骰子(請按任意按鍵): \n";
 		std::string temp;
 		std::cin >> temp;
-		aLoc += 17;
-		
-		if(aLoc%18 <= 5){
-			if(lastLoc!=0){
-				mapf[lastLoc%18] = '○';
-			}
-			mapf[(aLoc*3)%18] = '※';
-			lastLoc=aLoc*3;
-			printMap();
-		}else if(aLoc%18 == 6 || aLoc%18 == 17){
-			if(lastLoc!=0){
-				mapf[lastLoc%18] = '○';
-			}
-			if(aLoc%18 == 6){
-				map2[0]='○';
-			}else{
-				map2[10]='○';
-			}
-			lastLoc=aLoc;
-			printMap();
+		aLoc += dice(6);
+		if(lastLoc!=0){
+			map[(lastLoc)%25*2]='O';
 		}
+		map[(aLoc)%25*2]='A';
+		lastLoc=aLoc;
+		printMap();
+		
+		std::cout << "輪到B玩家擲骰子(請按任意按鍵): \n";
+	
+		std::cin >> temp;
+		bLoc += dice(6);
+		if(lastLoc2!=0){
+			map[(lastLoc2)%25*2]='O';
+		}
+		map[(bLoc)%25*2]='B';
+		lastLoc2=bLoc;
+		printMap();
+		std::cout<<std::endl;
 	}
 	
 	
